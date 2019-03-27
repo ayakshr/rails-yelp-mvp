6a2142a29d03e81447eb506f736cf5ffba3cbad1
Aya Kashihara <aya.kashihara@gmail.com>
2019-03-28 01:47:15 +0900

    routing, controllers and views implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  resources :reviews, only: [:show, :edit, :update, :destroy]
+
+  # get   'restaurants',      to: 'restaurants#index'
+
+  # get   'restaurants/new',  to: 'restaurants#new'
+  # post  'restaurants',      to: 'restaurants#create'
+
+  # get   'restaurants/:id',  to: 'restaurants#show'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'efish',
+    address:      'Gojo Kawabata',
+    phone_number: '1234-5678',
+    category:     'italian'
+  },
+  {
+    name:         'rakusenroh',
+    address:      'Shijo Karasuma',
+    phone_number: '1234-8765',
+    category:     'chinese'
+  },
+  {
+    name:         'kiccho',
+    address:      'Kyoto station',
+    phone_number: '5678-5678',
+    category:     'japanese'
+  },
+  {
+    name:         'french restaurant',
+    address:      'Gojo Karasuma',
+    phone_number: '8765-4321',
+    category:     'french'
+  },
+  {
+    name:         'belgian bar',
+    address:      'Sannomiya',
+    phone_number: '1234-4321',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
